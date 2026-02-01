310b27945856aa36412a3c30ffd8c5984354dcc4
Adrian Carl Dimaano <ai22m033@technikum-wien.at>
2026-02-01 21:32:14 +0100

    Reset

--- src/components/ui/Button.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Button({children, onClick, kind}:{children:React.ReactNode,onClick?:()=>void,kind?:'primary'|'default'}){
-  return (
-    <button className={`btn ${kind==='primary'?'primary':''}`} onClick={onClick}>{children}</button>
-  )
-}
--- src/components/ui/Modal.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Modal({ children, onClose }:{ children:React.ReactNode, onClose?:()=>void }){
-  return (
-    <div className="modal-backdrop" onClick={(e)=>{ if (e.target === e.currentTarget) onClose && onClose() }}>
-      <div className="modal">{children}</div>
-    </div>
-  )
-}
